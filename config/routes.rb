cf68fc03da8e236ed65894737eaed6eff9cf15ab
PK Banks <pkbanks12@gmail.com>
2019-02-06 16:56:10 -0500

    knock and foreman gems for authentication and running app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
   scope '/api' do
     resources :bananas
+    post 'user_token' => 'user_token#create'
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
