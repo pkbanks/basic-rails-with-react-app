a2a762c031855114dc149dd4294ff8f09d816517
PK Banks <pkbanks12@gmail.com>
2019-02-06 16:31:36 -0500

    user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_204030) do
+ActiveRecord::Schema.define(version: 2019_02_06_212423) do
 
   create_table "bananas", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
